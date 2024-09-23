5156b8c7bede5d60caedd2d8f1926a5b36dd52ad
a.sovetkin <a.sovetkin@rambler-co.ru>
2024-09-24 01:32:17 +0300

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min;
+        this.max;
+        this.center;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.center = this.min + Math.round((this.max - this.min) / 2);
+        return this.center;
     }
 
     lower() {
-
+        this.max = this.center;
     }
 
     greater() {
-
+        this.min = this.center;
     }
 }
 
